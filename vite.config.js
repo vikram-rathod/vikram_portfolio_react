e1b19e63ba1f74a26e91d95b745029fb26894040
Vikram Rathod <vikram.rathod@comittechnology.com>
2026-03-10 12:09:51 +0530

    updated issue  fixed

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-    plugins: [react()],
-    preview: {
-        allowedHosts: ['vikram-portfolio-rxs9.onrender.com'],
-    },
-})
-=======
   plugins: [react()],
   preview: {
-    allowedHosts: 'all',
+    allowedHosts: ['vikram-portfolio-rxs9.onrender.com'],
   },
 })
->>>>>>> 8da6790a8169203633d0b1bc3e78de11ecaf5070
